742122a9254a844bd4957017994fb416cebbaad3
austinjs <ajsprunger@gmail.com>
2022-05-10 13:03:29 -0600

    solve roman to int

--- solutions.js
+++ solutions.js
@@ -19,21 +19,40 @@
 
 // valid parentheses 
 
-function validParentheses(parens) {
-  let stack = [];
-  let map = {
-    '(':')',
-    '[':']',
-    '{':'}'
-  };
-  for (let i=0; i<parens.length; i++) {
-    if (stack.length > 0 && map[stack[stack.length - 1]] === parens[i]) {
-      stack.pop();
+// function validParentheses(parens) {
+//   let stack = [];
+//   let map = {
+//     '(':')',
+//     '[':']',
+//     '{':'}'
+//   };
+//   for (let i=0; i<parens.length; i++) {
+//     if (stack.length > 0 && map[stack[stack.length - 1]] === parens[i]) {
+//       stack.pop();
+//     } else {
+//       stack.push(parens[i])
+//     }
+//   }
+//   return stack.length === 0;
+// };
+
+var romanToInt = function(s) {
+  let romanMap = new Map();
+  romanMap.set('I', 1)
+  romanMap.set('V', 5)
+  romanMap.set('X', 10)
+  romanMap.set('L', 50)
+  romanMap.set('C', 100)
+  romanMap.set('D', 500)
+  romanMap.set('M', 1000)
+  let num = romanMap.get(s[s.length-1]);
+  for(let i=s.length-2; i >= 0; i--) {
+    if (romanMap.get(s[i]) >= romanMap.get(s[i+1])) {
+      num += romanMap.get(s[i])
     } else {
-      stack.push(parens[i])
+      num -= romanMap.get(s[i])
     }
   }
-  return stack.length === 0;
+  return num;
 };
 
-console.log(validParentheses('(((((((('))
