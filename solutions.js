bc318a43c7d9352e70d05b55051b0c96e101b964
austinjs <ajsprunger@gmail.com>
2022-05-12 11:24:32 -0600

    solve middleNode

--- solutions.js
+++ solutions.js
@@ -81,21 +81,29 @@
 //   return constructable;
 // };
 
-var fizzBuzz = function(n) {
-  let output = []
-  for (let i=1; i<=n; i++) {
-    if (i % 3 === 0 && i % 5 ===0) {
-      output.push("FizzBuzz")
-    } else if (i % 3 === 0) {
-      output.push("Fizz")
-    } else if (i % 5 === 0) {
-      output.push("Buzz")
-    } else {
-      output.push(i.toString())
-    }
+// var fizzBuzz = function(n) {
+//   let output = []
+//   for (let i=1; i<=n; i++) {
+//     if (i % 3 === 0 && i % 5 ===0) {
+//       output.push("FizzBuzz")
+//     } else if (i % 3 === 0) {
+//       output.push("Fizz")
+//     } else if (i % 5 === 0) {
+//       output.push("Buzz")
+//     } else {
+//       output.push(i.toString())
+//     }
+//   }
+//   return output;
+// };
+
+var middleNode = function(head) {
+  let fast = head
+  let slow = head
+  
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
   }
-  return output;
+  return slow;
 };
-
-
-
