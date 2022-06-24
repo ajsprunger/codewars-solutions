5d10977a5fdf422d4beb221fc25bb3414db4c783
austinjs <ajsprunger@gmail.com>
2022-06-24 12:46:27 -0600

    solve range sum query immutable

--- solutions.js
+++ solutions.js
@@ -743,10 +743,37 @@
 //   return true;
 // };
 
-var canWinNim = function(n) {
-  return n % 4 ? true : false
+// var canWinNim = function(n) {
+//   return n % 4 ? true : false
+// };
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+  this.nums = nums;
 };
 
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+  let sum = 0
+  for(let i=left; i<=right; i++) {
+    sum += this.nums[i]
+  }
+  return sum
+};
 
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
 
+var obj = new NumArray([-2, 0, 3, -5, 2, -1])
+var param_1 = obj.sumRange([0,2])
 
+obj.sumRange(0, 2)
